b4851fa625d5b40d6d8d4f55f4bb76bfb4c0edd1
Dean Agan <agandfr@gmail.com>
2021-12-23 16:24:18 +1100

    Clean up main

--- vue3-version/src/main.js
+++ vue3-version/src/main.js
@@ -2,26 +2,4 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-
-//import hljs from 'highlight.js/lib/common';
-//import "../node_modules/highlight.js/styles/monokai";
-
-
-
-// import VueHighlightJS from 'vue-highlight.js';
-
-// Highlight.js languages (All languages)
-// import 'vue-highlight.js/lib/allLanguages'
-
-/*
-* Import Highlight.js theme
-* Find more: https://highlightjs.org/static/demo/
-*/
-
-
-
-
-
-
-
 createApp(App).use(router).mount('#app')
