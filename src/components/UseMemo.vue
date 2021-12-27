c9787aa7fee5f42761d46d59129d36de1725720f
Dean Agan <agandfr@gmail.com>
2021-12-27 20:42:24 +1100

    Partial usememo work

--- src/components/UseMemo.vue
+++ src/components/UseMemo.vue
@@ -12,71 +12,19 @@ export default {
   data() {
     return {
       what: [
-        "useCallback returns a memoized callback",
-        "Returns a memoized version of the callback that only changes if the dependencies changed",
-        "Useful when passing callbacks to optimized child components that rely on reference equality \
-                    to prevent unnecessary renders (e.g. shouldComponentUpdate).",
-        "useCallback(fn, deps) is equivalent to useMemo(() => fn, deps).",
+        "useMemo returns a memoized value, caching a value so it isn't recalculated",
+        "Returns a memoized value that only changes if the dependencies changed",
+        "Addresses problems with referential equality and expensive computations",
       ],
-      mutablenotes: [
-        {
-          statement:
-            "Lambdas are equivalent to functors with const operator() by default.",
-          codes: [
-            dedentStrUsing1stLineIndent(`
-                    // This lambda
-                    auto productFn = [multiplier](int multiplicand) {
-                        return multiplicand * multiplier;
-                    };`),
-            dedentStrUsing1stLineIndent(`
-                    // is equivalent to this functor:
-                    struct Product {
-                        public:
-                            Product(int multiplier) : multiplier_(multiplier) {}
-                            int operator()(int multiplicand) const
-                            {
-                                return multiplicand * multiplier_;
-                            }
-                        private:
-                            int multiplier_;
-                    };`),
-          ],
-        },
-        {
-          statement:
-            "Lambdas follows the same syntax rules for const-qualified methods.",
-          codes: [
-            dedentStrUsing1stLineIndent(`
-                    auto someFn = [number](int anotherNumber) {
-                        ++number; // Error! expression must be modifiable lvalue
-                        return anotherNumber + number;
-                    };`),
-            dedentStrUsing1stLineIndent(`
-                    auto someFn = [number](int anotherNumber) mutable {
-                        ++number; // Now good due to mutable.
-                        return anotherNumber + number;
-                    };`),
-          ],
-        },
+      when: [
+        // Add code to demo cases
+      ],
+      downsides: [
+        // Add code to demo poor use
+        "Can affect performance thru excessive use in cases where operation is cheap.",
 
-        {
-          statement:
-            "If it were a reference though, it will still work as 'number' is now external to the object, and does not alter the object state.",
-          codes: [
-            dedentStrUsing1stLineIndent(`
-                    auto someFn = [&number](int anotherNumber) {
-                        ++number;
-                        return anotherNumber + number;
-                    };`),
-            dedentStrUsing1stLineIndent(`
-                    // Applies to pointers too!
-                    auto someFn = [pnumber](int anotherNumber) {
-                        ++(*pnumber);
-                        return anotherNumber + *pnumber;
-                    };`),
-          ],
-        },
       ],
+
     };
   },
 };
